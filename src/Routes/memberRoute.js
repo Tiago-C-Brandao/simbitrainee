aeef8139e7854c93ccfa886e2143503fc2b79d0c
Tiago-C-Brandao <tiago_curvelo@hotmail.com>
2023-03-13 11:47:29 -0300

    edit routers

--- src/Routes/memberRoute.js
+++ src/Routes/memberRoute.js
@@ -3,12 +3,12 @@ const members = require('../Controllers/memberController');
 const { verifyJWT } = require('../Services/VerifyJWT');
 const router = Router();
 
-router.get('/members', members.findAll);
+router.get('/members/find_all', members.findAll);
 
-router.post('/trainers/:trainer_id/members', members.addMember);
+router.post('/trainers/:trainer_id/members/creater', members.addMember);
 
-router.patch('/members/:member_id', members.updateMember);
+router.patch('/members/update/:member_id', members.updateMember);
 
-router.delete('/members/:member_id', members.deleteMember);
+router.delete('/members/delete/:member_id', members.deleteMember);
 
 module.exports = router;
--- src/Routes/trainerRoute.js
+++ src/Routes/trainerRoute.js
@@ -5,14 +5,14 @@ const members = require('../Controllers/memberController');
 const { verifyJWT } = require('../Services/VerifyJWT');
 const router = Router();
 
-router.get('/trainers', trainers.findAll);
-router.get('/trainers/:trainer_id', trainers.findTrainer);
+router.get('/trainers/find_all', trainers.findAll);
+router.get('/trainers/find_trainer/:trainer_id', trainers.findTrainer);
 
 
-router.post('/trainers', trainers.addTrainer);
+router.post('/trainers/creater', trainers.addTrainer);
 
-router.patch('/trainers/:trainer_id', trainers.updateTrainer);
+router.patch('/trainers/update/:trainer_id', trainers.updateTrainer);
 
-router.delete('/trainers/:trainer_id', trainers.deleteTrainer);
+router.delete('/trainers/delete/:trainer_id', trainers.deleteTrainer);
 
 module.exports = router;
