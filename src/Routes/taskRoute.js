cfa2b7e4464b9a43d99a73d76b0b8aa560436494
Tiago-C-Brandao <tiago_curvelo@hotmail.com>
2023-03-22 17:27:41 -0300

    create taskRoute

---
+++ src/Routes/taskRoute.js
@@ -0,0 +1,17 @@
+const { Router } = require('express');
+const tasks = require('../Controllers/taskController');
+const members = require('../Controllers/memberController');
+const trainer = require('../Controllers/trainerController');
+
+const router = Router();
+
+router.get('/tasks/find_all', tasks.findAll);
+router.get('/tasks/find_task/:task_id', tasks.findTask);
+
+router.post('/trainers/:trainer_id/members/:member_id/tasks/creater', tasks.addTask);
+
+router.patch('/trainers/:trainer_id/members/:member_id/tasks/update/:task_id', tasks.updateTask);
+
+router.delete('/trainers/:trainer_id/members/:member_id/tasks/delete/:task_id', tasks.deleteTask);
+
+module.exports = router;
